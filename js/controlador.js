8ad68e5f8ae4719044437ef5669119997fed0521
Luiz Eduardo <luizeduardovalle@gmail.com>
2016-07-01 17:40:48 -0300

    Gerar matriz de vizinhos e obter tabu funcionando!!

--- js/controlador.js
+++ js/controlador.js
@@ -82,4 +82,76 @@ angular.module('buscaTabu').controller('calculo', function ($scope) {
     };
 
 
+    /**
+     * Esse método inicia uma matriz de elementos já ordenando os vizinhos.
+     * @param {Array} solucaoTemporaria É um Array contendo a solução inicial.
+     * @returns {Array} Array contendo a distriuição dos "vizinhos".
+     */
+    function gerarMatrizVizinhos(solucaoTemporaria) {
+        var matrizVizinhos = [], aux, x;
+        for (x = 0; x < solucaoTemporaria.length; x++) {
+
+            aux = angular.copy(solucaoTemporaria);
+            aux[x] = (aux[x] === 0 ? 1 : 0);
+            matrizVizinhos.push(aux);
+        }
+        return matrizVizinhos;
+    }
+
+
+    /**
+     * 
+     * @param {Array} matrizVizinhos contendo a matriz com os "vizinhos" já ordenados
+     * @param {Array} objetos é um Array que contém os objetos e seus dados.
+     * @returns {Array} Retorna um Array contendo a matriz com os "vizinhos" e os dados de peso, benefício e f(s) calculados.
+     */
+    function obterResultado(matrizVizinhos, objetos) {
+
+        var i, k, valorPeso, valorBeneficio, valorSolucao;
+        for (i = 0; i < matrizVizinhos.length; i++) {
+            valorPeso = 0;
+            valorBeneficio = 0;
+            valorSolucao = 0;
+            for (k = 0; k < matrizVizinhos[i].length; k++) {
+
+                if (matrizVizinhos[i][k] === 1) {
+                    valorPeso += objetos[k].peso;
+                    valorBeneficio += objetos[k].beneficio;
+                }
+            }
+            valorSolucao = ((valorPeso - $scope.pesoMaximo) < 0 ? 0 : (valorPeso - $scope.pesoMaximo));
+            valorSolucao = valorBeneficio - $scope.alpha * (valorSolucao);
+
+            matrizVizinhos[i].push(valorBeneficio);
+            matrizVizinhos[i].push(valorPeso);
+            matrizVizinhos[i].push(valorSolucao);
+        }
+        return matrizVizinhos;
+    }
+
+
+    /**
+     * Define o "tabu" e "F(s" de maior valor positivo.
+     * @param {type} matrizVizinhos Recebe a matriz completa de "vizinhos".
+     * @returns {Object} Retorna um objeto contendo o "tabu" e "F(s)" da matriz de "vizinhos" passada.
+     */
+    function obterTabu(matrizVizinhos) {
+        var i, tabu = {};
+        tabu.max = 0;
+        for (i = 0; i < matrizVizinhos.length; i++) {
+            if (matrizVizinhos[i][matrizVizinhos[i].length - 1] > tabu.max) {
+                tabu.tabu = i + 1;
+                tabu.max = matrizVizinhos[i][matrizVizinhos[i].length - 1];
+            }
+        }
+
+        if (tabu.tabu === $scope.tabu) {
+            matrizVizinhos[tabu.tabu - 1][matrizVizinhos[0].length - 1] = 0;
+            tabu = obterTabu(matrizVizinhos);
+        }
+        return tabu;
+    }
+
+
+    
 });
